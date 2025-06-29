f2b24ce6f089f90c9cd6141d00592881493eb1fa
Rounak Dey <rounakriju2003@gmail.com>
2025-06-30 00:28:48 +0530

    Add files via upload

--- src/js/components/Canvas.js
+++ src/js/components/Canvas.js
@@ -7,17 +7,17 @@ import AutoBind from "../utils/bind";
 export default class Canvas {
   constructor() {
     this.images = [
-      "./img/1.jpg",
-      "./img/2.jpg",
-      "./img/3.jpg",
-      "./img/4.jpg",
-      "./img/5.jpg",
-      "./img/6.jpg",
-      "./img/7.jpg",
-      "./img/8.jpg",
-      "./img/9.jpg",
-      "./img/10.jpg",
-      "./img/11.jpg",
+      "/img/1.jpg",
+      "/img/2.jpg",
+      "/img/3.jpg",
+      "/img/4.jpg",
+      "/img/5.jpg",
+      "/img/6.jpg",
+      "/img/7.jpg",
+      "/img/8.jpg",
+      "/img/9.jpg",
+      "/img/10.jpg",
+      "/img/11.jpg",
     ];
 
     this.scroll = {
--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -9,7 +9,8 @@ uniform float uHoverIntensity;
 uniform float uBlackoutIntensity;
 uniform float uIsHovered;
  
-varying vec2 vUv; Random function for manga-style noise
+varying vec2 vUv;
+
 float random(vec2 st) {
     return fract(sin(dot(st.xy, vec2(12.9898,78.233))) * 43758.5453123);
 }
